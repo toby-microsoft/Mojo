cde665e23fd2936892854ce07ec4a1522cf035c0
Toby Hwang <tohwang@microsoft.com>
2018-07-24 20:50:47 -0700

    added login feature

--- app/app/app/App.xaml.cs
+++ app/app/app/App.xaml.cs
@@ -1,3 +1,4 @@
+using app.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,7 +12,7 @@ namespace app
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new LoginPage();
 		}
 
 		protected override void OnStart ()
