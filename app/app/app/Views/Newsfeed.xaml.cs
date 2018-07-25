c60c749ee660b5f023de8bc3fea83c761f3470eb
Toby Hwang <tohwang@microsoft.com>
2018-07-24 22:04:05 -0700

    build transistion feature

---
+++ app/app/app/Views/Newsfeed.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Newsfeed : ContentPage
+	{
+		public Newsfeed ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
